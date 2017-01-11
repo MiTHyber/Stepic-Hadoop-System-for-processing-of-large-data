30327975a885674ed81dfad0f1f97849ead9e9b8
MiTHyber <shevva1304@yahoo.com>
2017-01-11 17:56:38 +0300

    upd

---
+++ src/PageRankReducerV2.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class PageRankReducerV2 {
+    public static final int N = 5;
+    public static final double ALPHA = 0.1;
+
+    public static void main(String[] args) throws IOException {
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+            String cur = null;
+            Map<String,String[]> res = new LinkedHashMap<>();
+            while ((cur = br.readLine()) != null) {
+                String[] t = cur.split("\t");
+                res.putIfAbsent(t[0], new String[2]);
+                String[] v = res.get(t[0]);
+                double sum = 0;
+                if (v[0] != null)
+                    sum = Double.parseDouble(v[0]);
+                if (t[2].length() != 2)
+                    v[1] = t[2];
+                else
+                    sum += Double.parseDouble(t[1]);
+                v[0] = String.format("%.3f",sum);
+            }
+            for (Map.Entry<String, String[]> entry : res.entrySet()) {
+                System.out.printf("%s\t%.3f\t%s\n",entry.getKey(),ALPHA*(1d/N)+(1-ALPHA)*Double.parseDouble(entry.getValue()[0]),entry.getValue()[1]);
+            }
+        }
+    }
+}
